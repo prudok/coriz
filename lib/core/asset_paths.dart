654807959f970a6c5d2df7ddb77cfda6160d0a6a
rel1nce <romanprudchen@gmail.com>
2023-08-10 12:21:41 +0300

    style: add fixes

--- lib/core/asset_paths.dart
+++ lib/core/asset_paths.dart
@@ -1,9 +1,10 @@
-class AssetPaths {
+abstract class AssetPaths {
   static const String sleepingDogPath = 'assets/images/sleeping_dog.png';
   static const String chillRelaxPath = 'assets/gifs/chill_relax.gif';
-  static const String sleepingWithPillowPath = 'assets/gifs/sleeping_with_pillow.gif';
+  static const String sleepingWithPillowPath =
+      'assets/gifs/sleeping_with_pillow.gif';
   static const String thankYouPath = 'assets/gifs/thank_you.gif';
-  static const String iKnowAnswerPath= 'assets/gifs/i_know_answer.gif';
+  static const String iKnowAnswerPath = 'assets/gifs/i_know_answer.gif';
   static const String thinkingDogPath = 'assets/gifs/thinkingDog.gif';
   static const String clickDogPath = 'assets/gifs/click.gif';
 }
